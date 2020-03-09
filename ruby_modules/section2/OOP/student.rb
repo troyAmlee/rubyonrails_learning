53ed64369a3c1b06284181558c6904b12e1760a4
troyAmlee <troyamlee19@gmail.com>
2020-03-08 19:44:07 -0500

    Started on OOP

---
+++ ruby_modules/section2/OOP/student.rb
@@ -0,0 +1,22 @@
+class Student
+  attr_accessor :first_name, :last_name, :email, :username, :password
+  def initialize(first_name, last_name, email, username, password)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @username = username
+    @password = password
+  end
+
+  def to_s
+    print "First name = #{@first_name}\n"
+    print "Last name = #{@last_name}\n"
+    print "email = #{@email}\n"
+    print "username = #{@username}\n" 
+    print "password = #{@password}\n"
+  end
+
+end
+
+troy = Student.new("Troy", "Amlee", "hb0390am@go.somestate.edu", "Akicita_Sunka_Wakan_Ska", "sgdnlgfdsjgdfngjnn35325245###")
+puts troy
