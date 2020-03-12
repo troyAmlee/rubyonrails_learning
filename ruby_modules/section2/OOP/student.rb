2c43bf1b3a4a764ed879a81a92e2bc8e2085da00
troyAmlee <troyamlee19@gmail.com>
2020-03-11 21:31:41 -0500

    Added second class object

--- ruby_modules/section2/OOP/student.rb
+++ ruby_modules/section2/OOP/student.rb
@@ -12,11 +12,23 @@ class Student
     print "First name = #{@first_name}\n"
     print "Last name = #{@last_name}\n"
     print "email = #{@email}\n"
-    print "username = #{@username}\n" 
+    print "username = #{@username}\n"
     print "password = #{@password}\n"
   end
 
 end
 
-troy = Student.new("Troy", "Amlee", "hb0390am@go.somestate.edu", "Akicita_Sunka_Wakan_Ska", "sgdnlgfdsjgdfngjnn35325245###")
+troy = Student.new('Troy',
+                   'Amlee',
+                   'hb0390am@go.somestate.edu',
+                   'Akicita_Sunka_Wakan_Ska',
+                   'sgdnlgfdsjgdfngjnn35325245###')
+mike = Student.new('Mike',
+                   'Meska',
+                   'michaelmeska@hotmail.com',
+                   'meska1',
+                   'sdflkgsdfngkjsdfgksjd')
+
 puts troy
+
+puts mike
